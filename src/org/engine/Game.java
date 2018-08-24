0658e9f733273835aa09f7f845672249f8bff324
??? ??? <whiterobot210@gmail.com>
2018-08-24 18:34:03 -0400

    Accually did rendering and fullscreen

--- src/org/engine/Game.java
+++ src/org/engine/Game.java
@@ -5,14 +5,16 @@ import java.awt.Dimension;
 import org.gfx.Render;
 
 public class Game {
+	
+	public static boolean running = true;
 
 	public static void main(String[] args) {
-		Render.init(new Dimension(16,12));
+		Render.init(new Dimension(160,120));
 
 	}
 
 	public static void quit() {
-		// TODO Auto-generated method stub
+		running = false;
 		
 	}
 
--- src/org/gfx/Render.java
+++ src/org/gfx/Render.java
@@ -1,14 +1,17 @@
 package org.gfx;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.VolatileImage;
 
 import org.engine.Game;
 
@@ -19,7 +22,7 @@ public class Render {
 
 	private static Dimension canvasSize;
 	private static Dimension gameSize;
-	
+
 	private static int sizeFactor = 0;
 
 	public static void init(Dimension game_size) {
@@ -34,15 +37,13 @@ public class Render {
 		canvas = new Canvas();
 
 		canvas.setPreferredSize(canvasSize);
-		
+
 		makeFullscreen();
 
 		frame.add(canvas);
 		frame.pack();
 		frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
-		
-		
 
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
@@ -53,26 +54,73 @@ public class Render {
 
 		frame.setVisible(true);
 
+		startRendering();
+
+	}
+
+	private static void startRendering() {
+		Thread thread = new Thread() {
+			GraphicsConfiguration gc = canvas.getGraphicsConfiguration();
+			VolatileImage vImage = gc.createCompatibleVolatileImage(gameSize.width, gameSize.width);
+
+			public void run() {
+				while (Game.running) {
+					if (vImage.validate(gc) == VolatileImage.IMAGE_INCOMPATIBLE) {
+						vImage = gc.createCompatibleVolatileImage(gameSize.width, gameSize.height);
+					}
+
+					Graphics g = vImage.getGraphics();
+
+					g.setColor(Color.black);
+					g.fillRect(0, 0, gameSize.width, gameSize.height);
+					g.setColor(Color.red);
+					g.fillRect(1, 1, 119, 119);
+
+					g.dispose();
+
+					g = canvas.getGraphics();
+
+					System.out.println(sizeFactor + " " + canvasSize.width + " " + canvasSize.height);
+
+					g.drawImage(vImage, 0, 0, gameSize.width*sizeFactor, gameSize.height*sizeFactor, null);
+
+					g.dispose();
+
+					try {
+						sleep(100);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		thread.setName("Renderer");
+		thread.start();
+
 	}
 
 	private static void makeFullscreen() {
 		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		GraphicsDevice gd = env.getDefaultScreenDevice();
-		
-		if(gd.isFullScreenSupported()) {
+
+		if (gd.isFullScreenSupported()) {
 			frame.setUndecorated(true);
+			gd.setFullScreenWindow(frame);
 		}
-		
+
 	}
 
 	private static void getBestSize(Dimension screenSize) {
-		sizeFactor=0;
-		while (canvasSize.width < screenSize.width + gameSize.width
-				&& canvasSize.height < screenSize.height + gameSize.height) {
+		sizeFactor = 0;
+		while (screenSize.width > canvasSize.width + gameSize.width
+				&& screenSize.height > canvasSize.height + gameSize.height) {
 			canvasSize = new Dimension(canvasSize.width + gameSize.width, canvasSize.height + gameSize.height);
+			System.out.println(canvasSize.width + " " + canvasSize.height);
+			System.out.println("test " + screenSize.width + " " + screenSize.height);
 			sizeFactor++;
 		}
-		canvasSize = screenSize;
+		//canvasSize = screenSize;
 
 	}
 
