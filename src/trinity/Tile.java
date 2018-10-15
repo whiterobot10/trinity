aefcb5e36fca67f4e33a01baf9ae8228b7f24dac
??? ??? <whiterobot210@gmail.com>
2018-10-15 15:20:37 -0400

    who knows?

--- src/trinity/Tile.java
+++ src/trinity/Tile.java
@@ -4,23 +4,21 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-public class Tile {
+abstract public class Tile {
 
-	protected Twin pos;
+	protected final Twin pos;
 	protected byte[] offset = new byte[4];
-	protected int size;
-	protected int layer;
-	protected BufferedImage tileset;
 
-	protected String[] connectableIds;
-	protected String id;
+	protected Tile(Twin pos) {
+		this.pos = pos;
+	}
 
 	public void Update() {
 		boolean[] foo = new boolean[8];
 
-		for (int i = 0; i < connectableIds.length; i++) {
-			System.out.println("foo " + connectableIds[i]);
-			if (connectableIds[i] == null) {
+		for (int i = 0; i < getConnectableIds().length; i++) {
+			System.out.println("foo " + getConnectableIds()[i]);
+			if (getConnectableIds()[i] == null) {
 
 				for (int i2 = 0; i2 < 8; i2++) {
 					foo[i2] = true;
@@ -32,48 +30,48 @@ public class Tile {
 
 			if (t.pos.ix() == pos.ix() - 1 && t.pos.iy() == pos.iy() - 1) {
 				foo[0] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[0] = true;
 					}
 				}
 			}
 			if (t.pos.ix() == pos.ix() && t.pos.iy() == pos.iy() - 1) {
 				foo[1] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[1] = true;
 					}
 				}
 			}
 			if (t.pos.ix() == pos.ix() + 1 && t.pos.iy() == pos.iy() - 1) {
 				foo[2] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[2] = true;
 					}
 				}
 			}
 			if (t.pos.ix() == pos.ix() - 1 && t.pos.iy() == pos.iy()) {
 				foo[3] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[3] = true;
 					}
 				}
 			}
 			if (t.pos.ix() == pos.ix() + 1 && t.pos.iy() == pos.iy()) {
 				foo[4] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[4] = true;
 					}
 				}
 			}
 			if (t.pos.ix() == pos.ix() - 1 && t.pos.iy() == pos.iy() + 1) {
 				foo[5] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[5] = true;
 					}
 				}
@@ -81,16 +79,16 @@ public class Tile {
 			if (t.pos.ix() == pos.ix() && t.pos.iy() == pos.iy() + 1) {
 				System.out.println("hi");
 				foo[6] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[6] = true;
 					}
 				}
 			}
 			if (t.pos.ix() == pos.ix() + 1 && t.pos.iy() == pos.iy() + 1) {
 				foo[7] = false;
-				for (int i = 0; i < connectableIds.length; i++) {
-					if (t.id.equals(connectableIds[i])) {
+				for (int i = 0; i < getConnectableIds().length; i++) {
+					if (t.getId().equals(getConnectableIds()[i])) {
 						foo[7] = true;
 					}
 				}
@@ -100,7 +98,6 @@ public class Tile {
 		for (int i = 0; i < 8; i++) {
 			System.out.println(i + " " + foo[i]);
 		}
-		System.out.println(id);
 
 		bar(foo, 1, 3, 0, 0);
 		bar(foo, 4, 1, 2, 1);
@@ -109,37 +106,32 @@ public class Tile {
 
 	}
 
-	public Tile() {
-		this(0, 0);
-	}
+	public abstract BufferedImage getImage();
 
-	public Tile(int x, int y) {
-		this.pos = new Twin(x, y);
-		size = 8;
-		layer = 0;
-		tileset = Level.images.get("tileset.null");
-		connectableIds = new String[1];
-		connectableIds[0] = "null";
-		id = "null";
+	public abstract String[] getConnectableIds();
 
-	}
+	public abstract String getId();
+
+	public abstract int getSize();
+
+	public abstract int getLayer();
 
 	public void draw(Graphics2D g, int layer) {
 
-		if (layer == this.layer) {
-			draw(g, pos, offset[0], offset[1], offset[2], offset[3], tileset, size, layer);
+		if (layer == getLayer()) {
+			draw(g, pos, offset[0], offset[1], offset[2], offset[3], getImage(), getSize(), layer);
 		}
 	}
 
 	public static void draw(Graphics2D g, Twin pos, int offset0, int offset1, int offset2, int offset3,
-			BufferedImage tileset, int size, int layer) {
+			BufferedImage image, int size, int layer) {
 		int foo = (size + 1) / 2;
-		g.drawImage(tileset.getSubimage(size * offset0, 0, foo, foo), pos.ix() * size, pos.iy() * size, null);
-		g.drawImage(tileset.getSubimage(size * offset1 + foo, 0, foo, foo), pos.ix() * size + size / 2, pos.iy() * size,
+		g.drawImage(image.getSubimage(size * offset0, 0, foo, foo), pos.ix() * size, pos.iy() * size, null);
+		g.drawImage(image.getSubimage(size * offset1 + foo, 0, foo, foo), pos.ix() * size + size / 2, pos.iy() * size,
 				null);
-		g.drawImage(tileset.getSubimage(size * offset3, foo, foo, foo), pos.ix() * size, pos.iy() * size + size / 2,
+		g.drawImage(image.getSubimage(size * offset3, foo, foo, foo), pos.ix() * size, pos.iy() * size + size / 2,
 				null);
-		g.drawImage(tileset.getSubimage(size * offset2 + foo, foo, foo, foo), pos.ix() * size + size / 2,
+		g.drawImage(image.getSubimage(size * offset2 + foo, foo, foo, foo), pos.ix() * size + size / 2,
 				pos.iy() * size + size / 2, null);
 
 	}
